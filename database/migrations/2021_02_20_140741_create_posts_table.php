f710b8ace52880277da37021cb839c7b3c679b9f
ALEX\olari <alex.o@duk-tech.com>
2021-02-20 17:03:24 +0200

    Project startup

---
+++ database/migrations/2021_02_20_140741_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->default(null);
+            $table->string('url')->default(null);
+            $table->text('description')->default(null);
+            $table->string('tag')->default(null);
+            $table->text('hashtags')->default(null);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
---
+++ resources/js/Components/mine/NewPost.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+    <v-input
+    v-model:>
+
+    </v-input>
+</div>
+</template>
+
+<script>
+export default {
+    name: "NewPost",
+    data(){
+        return{
+            post: {
+                title: "",
+                url: "",
+                description: "",
+                tag:"",
+                hashtags:"",
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
